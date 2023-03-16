b01a9d2b1fe57743f9c10b063cf8ad971ff6167f
ananthu <ananthu4uuu@gmail.com>
2023-03-17 00:08:48 +0530

    bug:Mar/17/12.8am

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const http = require('http');
 const { Server } = require('socket.io')
 const cors = require('cors')
 const fs = require('fs');
-// const url = 'mongodb+srv://chitchat:FxB1fHTmqvVFct9z@cluster0.horg3tw.mongodb.net/?retryWrites=true&w=majority'; // Connection URL
-// const db = require('monk')(url);
-// const collectionUser = db.get('users')
-// const collectionActiveUsers = db.get('activeUsers')
-// const { sessionMiddlewear, wrap } = require('./session/sessionMiddlewear')
 const cookieParser = require("cookie-parser");
 const multer = require('multer')
 const port = 3000
@@ -19,11 +14,6 @@ const port = 3000
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(cors({
-//     origin: 'http://localhost:3000',
-//     credentials: true,
-// }));
-// app.use(sessionMiddlewear)
 app.use(cookieParser());
 app.use(express.static('build'));
 app.use("/public", express.static(path.join(__dirname, 'public')));
@@ -53,8 +43,6 @@ const io = new Server(server, {
     }
 })
 
-// io.use(wrap(sessionMiddlewear))
-
 
 let activeUser = [{ Name: 'Manu', Genter: 'male', imageURL: '' }]
 let login = false
@@ -65,43 +53,10 @@ app.post('/login', async (req, res) => {
     console.log('login...');
     const user = activeUser.find((item => item.Name === req.body.Name))
     if (user) return res.send({ msg: 'Name is already used!' })
-    // const user = req.body
-
-    // activeUser.push(req.body)
     login = true
     res.send(req.body)
 })
 
-// app.post('/signup', async (req, res) => {
-//     console.log('/signup');
-//     await collectionUser.findOne({ email: req.body.email }).then(async (doc) => {
-//         if (!doc) {
-//             await collectionUser.insert(req.body)
-//                 .then((docs) => {
-//                     res.send({ userCreated: true, data: docs.id })
-//                 }).catch((err) => {
-//                     res.send({ error: err })
-//                 }).then(() => db.close())
-//         } else {
-//             res.send({ userCreated: false })
-//         }
-//     })
-// })
-
-
-// app.get('/auth', async (req, res) => {
-//     console.log('/auth');
-//     if (req.session.loginAuth) {
-//         await collectionUser.findOne({ _id: req.session.user._id }, { projection: { firstName: 1, _id: 1, imageUrl: 1 } }).then((doc) => {
-//             req.session.user = doc
-
-//             res.send({ loginGranted: req.session.loginAuth, user: req.session.user })
-//         })
-
-
-//     }
-
-// })
 
 app.post('/logout', async (req, res) => {
     console.log('/logout');
@@ -109,29 +64,13 @@ app.post('/logout', async (req, res) => {
     const array = activeUser.filter(item => item.Name !== req.body.Name)
     activeUser = [...array]
     console.log(activeUser);
-    // let data = req.session.user
-
-    // await collectionActiveUsers.remove({ userName: data.firstName }).then((doc) => {
-    //     login = false
-    //     res.send({ logoutGranted: true })
-    // })
-    // await collectionActiveUsers.remove({ userId: activeUser.userId }).then((doc) => {
-    //     login = false
-    //     req.session.destroy()
+    
     res.send({ logoutGranted: true })
-    // })
+    
 
 })
 
-// app.get('/activeUsers', async (req, res) => {
-//     console.log('/activeUser');
-//     if (req.session.loginAuth) {
-//         await collectionActiveUsers.find().then((doc) => {
-//             console.log('active:' + doc);
-//             res.send(doc)
-//         })
-//     }
-// })
+
 
 // app.post('/imageUpdate', async (req, res) => {
 //     console.log('image uploading');
@@ -244,28 +183,14 @@ io.on('connection', async (socket) => {
         console.log('typing...' + type)
     })
 
-    // socket.on('live', (data) => console.log(data))
 
     socket.on('newUser', async (data) => {
         console.log('newUser');
         let Data = { ...data, socketID: socket.id }
         const oldUser = activeUser.find((item) => item.Name === data.Name)
         if (!oldUser) activeUser.push(Data)
-        // console.log(activeUser);
-        // activeUser.push(data)
-
-        // const array=[...new Set(activeUser)]
         socket.emit('activeUser', activeUser)
-        // console.log();
-        // await collectionActiveUsers.findOne({ userId: data.userId }).then(async (doc) => {
-        //     if (!doc) {
-        //         await collectionActiveUsers.insert(data).then((doc) => {
-
-        //         }).catch((err) => {
-        //             res.send({ error: err })
-        //         }).then(() => db.close())
-        //     }
-        // })
+        
     })
 
     socket.on('disconnect', async () => {
@@ -274,7 +199,6 @@ io.on('connection', async (socket) => {
         activeUser = [...array]
         // await collectionActiveUsers.remove({ userId: activeUser.userId }).then((doc) => {
         socket.disconnect();
-        // })
 
     });
 });
